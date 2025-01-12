3ded624e709a542d5f22f4da110451cfd3ff4788
Leonardo Baião <leobaiao966@gmail.com>
2025-01-12 17:02:04 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+         string entrada = Console.ReadLine();
+         string[] valores = entrada.Split(' ');
+            int a = int.Parse(valores[0]);
+            int b = int.Parse(valores[1]);
+            int c = int.Parse(valores[2]);
+            int d = int.Parse(valores[3]);
+
+            if (b > c && d > a && c+d > a+b && a % 2 == 0 && c > 0 && d > 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+
+
+        
+    }
+
+}
